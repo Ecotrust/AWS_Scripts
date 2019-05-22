97f083b351ed7633a2fb2dbf98018b93ae824977
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-05-21 18:15:45 -0700

    initial commit of snapshot script and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # AWS_Scripts
 Scripts to help automate managing AWS servers
+
+
+## Installation
+The current scripts are BASH shell scripts written for Linux.
+Be sure that they are executable, and then run with ./{scriptname}.sh
+Helper scripts may be written in Python. These were written for python 3.
+
+## Snapshot
+ > Requires awscli
+ > `sudo apt install awscli`
+ > requires python, relies on script 'parse_snapshots.py'
+
+### Configure:
+From your AWS instance you will need:
+ * The AWS Region where your instance is housed (i.e. 'us-west-2' for Oregon)
+ * Your Access Key ID (often starts like 'AKIA...')
+ * Your AWS Secret Access Key (you get this when you create a user)
+ * The Volume ID of the EBS volume you wish to back up ('vol-...')
+ * (Optional) The number of days of age at which point you wish to delete old backups
+    * This is set to 90 by default,
+
+### Run:
+Run with `./snapshot.sh`
---
+++ snapshot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export AWS_DEFAULT_REGION=us-west-2
+export AWS_ACCESS_KEY_ID="SET ME"
+export AWS_SECRET_ACCESS_KEY="SET ME"
+export AWS_VOLUME_ID="SET ME"
+export DELETE_AFTER_DAYS=90
+
+echo
+echo ========== BEGIN SNAPSHOT SCRIPT ==========
+process=`aws ec2 create-snapshot --volume-id $AWS_VOLUME_ID --description /dev/xvda1`
+pid=$(echo $process | perl -nle'print $& if m{"SnapshotId": ".*?[^\\]"}' | sed 's/\"SnapshotId\": \"//g' | sed 's/\"//g')
+echo creating snapshot $pid ...
+status=`aws ec2 describe-snapshots --snapshot-ids $pid`
+pct_done=$(echo $status | perl -nle'print $& if m{"Progress": ".*?[^\\]"}' | sed 's/\"Progress\": \"//g' | sed 's/\%"//g')
+while [[ $pct_done != "100" ]]
+do
+    echo $pct_done% completed...
+    sleep 10
+    status=`aws ec2 describe-snapshots --snapshot-ids $pid`
+    pct_done=$(echo $status | perl -nle'print $& if m{"Progress": ".*?[^\\]"}' | sed 's/\"Progress\": \"//g' | sed 's/\%"//g')
+done
+echo snapshot $pid completed
+
+snapshots=`aws ec2 describe-snapshots --owner self --output json`
+
+### parse_snapshots.py takes the json list of snapshots and the age at
+###     which they are considered to be old in "DAYS"
+old_snaps=`python parse_snapshots.py "$snapshots" $DELETE_AFTER_DAYS`
+IFS="|" read -a old_array <<< "$old_snaps"
+
+for i in "${old_array[@]}"
+  do
+    echo ---------------------
+    echo DELETING "$i"...
+    aws ec2 delete-snapshot --snapshot-id $i
+    echo SNAPSHOT "$i" DELETED
+  done
+echo ========END SNAPSHOT SCRIPT ========
+echo
